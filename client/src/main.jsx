1a13d09202be88cadad90dac0daaab2bdf6da3e0
saintpbh <saintpbh@gmail.com>
2026-04-24 10:32:55 +0900

    Fix CORS issue with ngrok tunnel by injecting ngrok-skip-browser-warning in fetch requests

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,19 @@ import './index.css'
 import './report-table.css'
 import App from './App.jsx'
 
+// Add global fetch interceptor to bypass ngrok browser warning
+const originalFetch = window.fetch;
+window.fetch = async function (...args) {
+  let [resource, config] = args;
+  config = config || {};
+  config.headers = {
+    ...config.headers,
+    'ngrok-skip-browser-warning': '69420'
+  };
+  args[1] = config;
+  return originalFetch.apply(this, args);
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
